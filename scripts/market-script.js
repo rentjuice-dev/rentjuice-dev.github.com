579a7a66e51f8b74aacc5a28d887cf0bc1d02321
Gregg <gregg@rentjuice.com>
2012-09-05 11:13:48 -0700

    adding all files

---
+++ scripts/market-script.js
@@ -0,0 +1,132 @@
+(function(global, $) { var
+
+	$win, jpScreen, aptOne, aptTwo, scrollingGraphic,
+	dataEntryCircle, dataEntryAptOne, dataEntryAptTwo,
+	directoryAptOne, directoryAptTwo, sectionTopBrowserScreen,
+	sectionTopIcon, websitesBrowserScreen, websitesIcon,
+	bottom_position = '1630px', fixed_position = '190px', scroll_bottom = '1550';
+	
+	function onScroll() {
+		var topPos = $win.scrollTop();
+		
+		/* Set position as you scroll for scrolling element */
+		if(topPos >= scroll_bottom) {
+			scrollingGraphic.css({'position':'absolute','top':bottom_position})
+		} else if(topPos > 200 && topPos < scroll_bottom) {
+			scrollingGraphic.css({'position':'fixed','top':fixed_position})
+		} else if(topPos < 200) {
+			scrollingGraphic.css({'position':'absolute','top':fixed_position})
+		}
+		
+		/* Market Section Top » Browser Screen Switcher */
+		if((topPos > 0) && (topPos < 20)) {
+			sectionTopBrowserScreen.removeClass().addClass('facebook');
+			sectionTopIcon.removeClass().addClass('abs-img over facebook');
+		} else if((topPos > 20) && (topPos < 70)) {
+			sectionTopBrowserScreen.removeClass().addClass('zillow');
+			sectionTopIcon.removeClass().addClass('abs-img over zillow');
+		} else if(topPos > 120) {
+			sectionTopBrowserScreen.removeClass().addClass('craigslist');
+			sectionTopIcon.removeClass().addClass('abs-img over craigslist');
+		}
+		
+		/* 'All of Your Lisitngs' Rules */
+		if(topPos >= 280) {
+			dataEntryAptOne.css('opacity', '1').animate({
+				top: '0',
+				left: '180',
+				width: '120',
+				height: '120'
+			  }, {
+				duration: 300, 
+				easing: 'swing',
+				complete: function() { // the callback
+					dataEntryAptTwo.animate({
+						top: '100',
+						left: '260',
+						width: '150',
+						height: '150'
+					}, {
+						duration: 500, 
+						easing: 'swing'
+					});
+				}
+			  });
+			dataEntryAptTwo.css('opacity', '1');
+		}
+		
+		/* 'And listings from around town' Rules */
+		if(topPos >= 660) {
+			directoryAptOne.animate({
+				top: '0',
+			  }, {
+				duration: 1000, 
+				easing: 'easeOutElastic'
+			  });
+			directoryAptTwo.animate({
+				top: '170'
+			  }, {
+				duration: 1200, 
+				easing: 'easeOutElastic'
+			  });
+		}
+		
+		/* On all your websites » Browser Screen Switcher */
+		if((topPos > 0) && (topPos < 1030)) {
+			websitesBrowserScreen.removeClass().addClass('rentsfnow');
+			websitesIcon.css('opacity', '0');
+		} else if((topPos > 1030) && (topPos < 1250)) {
+			websitesBrowserScreen.removeClass().addClass('hotpads');
+			websitesIcon.removeClass().addClass('abs-img over hotpads').css('opacity', '1');
+		} else if(topPos > 1250) {
+			websitesBrowserScreen.removeClass().addClass('facebook');
+			websitesIcon.removeClass().addClass('abs-img over facebook').css('opacity', '1');
+		}
+		
+		/* Even if you dont... » Browser Screen Switcher */
+		if((topPos > 0) && (topPos < 1300)) {
+			jpScreen.removeClass().addClass('pre-juice');
+		} else if((topPos > 1300) && (topPos < 1400)) {
+			jpScreen.removeClass().addClass('mid-juice');
+		} else if (topPos > 1400) {
+			jpScreen.removeClass().addClass('post-juice');
+		}
+	}
+
+	function setupPage() {
+		jpScreen.removeClass().addClass('pre-juice');
+		aptOne.css('top', '-180px');
+		aptTwo.css('top', '-180px');
+		dataEntryCircle.css({'top':'190px','left':'180px','width':"0",'height':'0','opacity':"0"});
+	}
+
+	function cacheSelectors() {
+		$win = $(global);
+		jpScreen = $('#juicepress #screen');
+		aptOne = $('#directory #apt-one');
+		aptTwo = $('#directory #apt-two');
+		scrollingGraphic = $('#scrolling-graphic');
+
+		dataEntryCircle = $('#data-entry .circle');
+		dataEntryAptOne = $('#data-entry #apt-one');
+		dataEntryAptTwo = $('#data-entry #apt-two');
+
+		directoryAptOne = $('#directory #apt-one');
+		directoryAptTwo = $('#directory #apt-two');
+
+		websitesBrowserScreen = $('#websites #browser #screen');
+		websitesIcon = $('#websites #icon');
+
+		// section top
+		sectionTopBrowserScreen = $('#section-top #browser #screen');
+		sectionTopIcon = $('#section-top #icon');
+	}
+
+	function init() {
+		cacheSelectors();
+		setupPage();
+		$win.scroll(onScroll);
+	}
+
+	$(init);
+})(window, jQuery);
---
+++ scripts/retain-script.js
@@ -0,0 +1,55 @@
+$(function() {
+	
+});
+
+$(document).ready(function(){
+	
+	$('#alerts #alert-phone').removeClass().addClass('abs-img pre-phone over');
+	$('#alerts #alert-clock').removeClass().addClass('abs-img pre-clock under');
+	$('#in-touch #intouch-alert').css('opacity', '0');
+	$('#in-touch #mail').removeClass().addClass('abs-img pre-mail under');
+	
+	$(window).scroll(function() {
+		
+		/* Set positions for scrolling element */
+		var bottom_position = new String("1104px");
+		var fixed_position = new String("180px");
+		var scroll_bottom = new String("1120");
+		
+		/* Set position as you scroll for scrolling element */
+		if($(this).scrollTop() >= scroll_bottom)
+		{
+			$('#scrolling-graphic').css({'position':'absolute','top':bottom_position});
+		} else if($(this).scrollTop() < scroll_bottom) {
+			$('#scrolling-graphic').css({'position':'fixed','top':fixed_position});
+		}
+		
+		/* Alert Section » iPhone Bump */
+		if($(this).scrollTop() < 450)
+		{
+			$('#alerts #alert-phone').removeClass().addClass('abs-img pre-phone over');
+			$('#alerts #alert-clock').removeClass().addClass('abs-img pre-clock under');
+		} else if ($(this).scrollTop() >= 450)
+		{
+			$('#alerts #alert-phone').removeClass().addClass('abs-img post-phone over');
+			$('#alerts #alert-clock').removeClass().addClass('abs-img post-clock under');
+		}
+		
+		/* Capture New Leads » iPhone Bump */
+		if(($(this).scrollTop() > 0) && ($(this).scrollTop() < 657))
+		{
+			$('#in-touch #intouch-alert').css('opacity', '0');
+			$('#in-touch #mail').removeClass().addClass('abs-img pre-mail under');
+		} else if (($(this).scrollTop() > 657) && ($(this).scrollTop() < 846))
+		{
+			$('#in-touch #intouch-alert').css('opacity', '1');
+			$('#in-touch #mail').removeClass().addClass('abs-img mid-mail under');
+		} else if ($(this).scrollTop() > 846)
+		{
+			$('#in-touch #intouch-alert').css('opacity', '1');
+			$('#in-touch #mail').removeClass().addClass('abs-img post-mail under');
+		}
+	
+	});
+
+});
